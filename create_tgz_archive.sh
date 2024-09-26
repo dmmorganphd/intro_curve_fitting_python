504013fb1501e9a0bcbccf9d2b633069c01085f7
dmmorganphd <65974829+dmmorganphd@users.noreply.github.com>
2024-09-26 17:22:36 -0500

    once more from chez Lise

--- create_tgz_archive.sh
+++ create_tgz_archive.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/bash
-tar -zcvf ../intro_curve_fitting_python.tgz ./
 
-if [ $? != 0 ] ; then
-	echo "Something went wrong creating the archive."
-else
-	# epoch time
-	datestamp=`date +%s`
-	nfn='../intro_curve_fitting_python_'$datestamp'.tgz'
-	mv ../intro_curve_fitting_python.tgz $nfn
-	if [ $? !=0 ] ; then
-	      echo "Something went wrong renaming the archive."
-     	else
-		echo "Successfully created "$nfn
-	fi
-fi
+IFS='/'
+read -ra newarr <<< `pwd`
+
+# echo "there are "${#newarr[@]}" elements in newarr and the last element is: "${newarr[-1+${#newarr[@]}]}
+
+datestamp=`date +%s`
+nfn="..//"${newarr[-1+${#newarr[@]}]}
+
+echo $nfn
