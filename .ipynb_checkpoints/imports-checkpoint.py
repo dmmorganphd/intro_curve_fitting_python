f014c70efecbe53d99085a3f2121d83aae51bd81
dmmorganphd <65974829+dmmorganphd@users.noreply.github.com>
2024-10-04 15:31:57 -0500

    create tgz archive script updated to exclude git directories

---
+++ .ipynb_checkpoints/imports-checkpoint.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+get_ipython().run_line_magic('matplotlib', 'inline')
+from scipy.optimize import curve_fit
+import os
+
